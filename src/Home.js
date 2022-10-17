6b922942ab1232a2d191571a710d7df18407ef5a
daoson12 <daoduvictor48@gmail.com>
2022-10-17 14:50:52 +0000

    added error message

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,41 @@
 import React from 'react'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
-  const[isPending, setIsPending]= useState(true)
-
-  useEffect(()=>{
-setTimeout(() => {
-  fetch('http://localhost:8000/blog').then(
-    (res)=>{
-     return res.json();
-    })
-    .then(data=>{
-      console.log(data);
-      setBlogs(data)
-      setIsPending(false)
-    })
-}, 5000);
-  },[]);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blog')
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch a data from the resource")
+        }
+        console.log(res);
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          setBlogs(data)
+          setIsPending(false)
+          setError(null)
+        })
+        .catch(err=>{
+         setIsPending(false)
+         setError(err.message)
+        })
+    }, 1000);
+  }, []);
 
   return (
     <div className='home'>
-      {isPending && <div style={{color:'slateblue'}}>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!"  />}
+    {error && <div>{error}</div>}
+      {isPending && <div style={{ color: 'slateblue' }}>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   )
 }
